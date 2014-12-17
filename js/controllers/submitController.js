41bf917fa929424c0935ac4d7cdec8b2941a2c49
unknown <settyd@INENSETTYDL3C.corp.emc.com>
2014-12-17 06:03:47 +0530

    comitting verb addition changes

--- js/controllers/submitController.js
+++ js/controllers/submitController.js
@@ -1,17 +1,32 @@
 app.controller('submitCtrl',function($scope,httpService){
 	 $scope.details = {};
 	 $scope.response={};
-	
+	$scope.activity = {
+        
+        currentActivity: "POST"
+    };
+ 
+    $scope.activities =
+    [
+        "GET",
+        "POST",
+        "PUT",
+        "Delete"
+    ];  
+
+
+       
 	$scope.submit=function(details)
 	{
 		   
 		alert('url is '+$scope.details.apiUrl);
-		var res=httpService.getResponse($scope.details.apiUrl);
+		var res=httpService.getResponse($scope.details.apiUrl,$scope.activity.currentActivity);
 		
 		res.then(function(data)
 		{
 			console.log('result '+data.login);
-			$scope.details.response=data;
+			console.log('result '+data.toString());
+			$scope.details.response=JSON.stringify(data);
 		})
 	}
 
--- js/services/httpService.js
+++ js/services/httpService.js
@@ -3,15 +3,25 @@ app.service('httpService',function($http,$q)
 
 return {
 
-getResponse:function(apiUrl)
+getResponse:function(apiUrl,verb)
 {
 		
 			var deferred = $q.defer();
 
-			$http.get(apiUrl).success(function(data) {
+			var req = {
+							 method:verb,
+ 							 url: apiUrl,
+ 					  }
+
+
+						$http(req).success(function(data) {
+											deferred.resolve(data);
+										});
+
+			/*$http.get(apiUrl).success(function(data) {
 				deferred.resolve(data);
 			});
-
+*/
 			return deferred.promise;
 		}
 }
